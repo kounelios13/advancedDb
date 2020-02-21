1252c992202032afe2db02217de5965ad0ff4210
marcelom97 <marcelomollaj@gmail.com>
2020-02-21 14:14:24 +0200

    [added] lab ex node on lab

---
+++ 01-Introduction/Auctions-1_Scripts.sql
@@ -0,0 +1,8 @@
+﻿-- Να βρεθούν οι καλλιτέχνες που βρίσκονται εν ζωή
+-- {τίτλοι στηλών στην εκτύπωση: ΌνομαΚαλλιτέχνη, ΈτοςΓέννησης}.
+SELECT Name,YearOfBirth FROM ARTIST WHERE YearOfDeath IS NULL;
+
+-- Να βρεθούν τα έργα τέχνης τα οποία ανήκουν στον τύπο έργων τέχνης που ονομάζεται 'oil on panel'
+-- (δηλαδή ουσιαστικά αναζητούνται τα έργα που είναι πίνακες ζωγραφικής με την τεχνική «λάδι σε πάνελ»)
+-- {τίτλος στήλης στην εκτύπωση: ΟνομασίαΈργου}.
+SELECT * FROM ARTWORK inner join TYPEOFWORK on ARTWORK.TypeOfWork=TYPEOFWORK.TypeID WHERE TYPEOFWORK.Name='oil on panel';;
