6d0dfbafe92765737a352ed4d17ad5ac1598f6e6
marcelom97 <marcelomollaj@gmail.com>
2020-03-19 11:47:04 +0200

    [added] joinless decomp

---
+++ odeio/MusicSchool-With-3AttributeKey.sql
@@ -0,0 +1,53 @@
+﻿drop table if exists organo cascade;
+drop table if exists daskalos cascade;
+drop table if exists spoudastns cascade;
+drop table if exists xreosn cascade;
+
+create table organo (
+kodikos_organou text not null,
+onoma_organou text not null,
+primary key (kodikos_organou)
+);
+
+create table daskalos (
+kodikos_daskalou text not null,
+onoma_daskalou text not null,
+primary key (kodikos_daskalou)
+);
+
+create table spoudastns (
+kodikos_spoudastn text not null,
+onoma_spoudastn text not null,
+dieuthynsn text,
+primary key (kodikos_spoudastn)
+);
+
+create table xreosn (
+kodikos_daskalou text not null,
+kodikos_spoudastn text not null,
+kodikos_organou text not null,
+xreosn integer,
+foreign key (kodikos_daskalou) references daskalos (kodikos_daskalou),
+foreign key (kodikos_spoudastn) references spoudastns (kodikos_spoudastn),
+foreign key (kodikos_organou) references organo (kodikos_organou),
+primary key (kodikos_daskalou, kodikos_spoudastn, kodikos_organou)
+);
+
+insert into organo values ('ENX-1', 'Mpouzouki');
+insert into organo values ('ENX-2', 'Kithara');
+
+insert into daskalos values ('D-1', 'SERPANOS STELIOS');
+insert into daskalos values ('D-2', 'APOXHS GIANNIS');
+insert into daskalos values ('D-3', 'PAGKOS KOSTAS');
+
+insert into spoudastns values ('S-01', 'KOKOS GIANNIS', 'TEI KRHTHS');
+insert into spoudastns values ('S-02', 'KABOS LAZAROS', 'TEI KRHTHS');
+insert into spoudastns values ('S-03', 'LHMAS GIORGOS', 'TEI ATHENS');
+
+insert into xreosn values ('D-1', 'S-01', 'ENX-1', 17);
+insert into xreosn values ('D-2', 'S-02', 'ENX-1', 16);
+insert into xreosn values ('D-3', 'S-03', 'ENX-1', 17);
+insert into xreosn values ('D-1', 'S-03', 'ENX-1', 12);
+
+
+
---
+++ odeio/odeio-4tables-decomp-2.sql
@@ -0,0 +1,57 @@
+﻿drop table if exists organo cascade;
+drop table if exists daskalos cascade;
+drop table if exists spoudastns cascade;
+drop table if exists xreosn cascade;
+
+create table organo (
+kodikos_organou text not null,
+onoma_organou text not null,
+primary key (kodikos_organou)
+);
+
+create table daskalos (
+kodikos_daskalou text not null,
+onoma_daskalou text not null,
+kodikos_organou text not null,
+foreign key (kodikos_organou) references organo (kodikos_organou),
+primary key (kodikos_daskalou)
+);
+
+create table spoudastns (
+kodikos_spoudastn text not null,
+onoma_spoudastn text not null,
+dieuthynsn text,
+primary key (kodikos_spoudastn)
+);
+
+create table xreosn (
+kodikos_daskalou text not null,
+kodikos_spoudastn text not null,
+xreosn integer,
+foreign key (kodikos_daskalou) references daskalos (kodikos_daskalou),
+foreign key (kodikos_spoudastn) references spoudastns (kodikos_spoudastn),
+primary key (kodikos_daskalou, kodikos_spoudastn)
+);
+
+insert into organo values ('ENX-1', 'Mpouzouki');
+insert into organo values ('ENX-2', 'Kithara');
+
+insert into daskalos values ('D-1', 'SERPANOS STELIOS', 'ENX-1');
+insert into daskalos values ('D-2', 'APOXHS GIANNIS', 'ENX-1');
+insert into daskalos values ('D-3', 'PAGKOS KOSTAS', 'ENX-2');
+
+insert into spoudastns values ('S-01', 'KOKOS GIANNIS', 'TEI KRHTHS');
+insert into spoudastns values ('S-02', 'KABOS LAZAROS', 'TEI KRHTHS');
+insert into spoudastns values ('S-03', 'LHMAS GIORGOS', 'TEI ATHENS');
+
+insert into xreosn values ('D-1', 'S-01', 17);
+insert into xreosn values ('D-2', 'S-02', 16);
+insert into xreosn values ('D-3', 'S-03', 17);
+insert into xreosn values ('D-1', 'S-03', 12);
+
+select * from organo;
+select * from daskalos;
+select * from spoudastns;
+select * from xreosn;
+
+
